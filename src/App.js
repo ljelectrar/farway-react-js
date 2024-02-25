4ccccd8e016adf700eebb7a26ef7d0a2e4249943
ljelectrar <ljelectrar@gmail.com>
2024-02-25 09:43:03 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true  },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="app">
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+  </div>
   );
 }
 
-export default App;
+function Logo(){
+  return <h1>😘 🌴 Far Away 🌴 😘</h1>
+}
+
+function Form(){
+  return ( 
+    <form className="add-form">
+      <h3>What do you need for your 😒 trip?</h3>
+      <select>
+        {Array.from({length: 20}, (_, i) => i  + 1)
+        .map(num=>
+        <option value={num} key={num}>
+          {num}
+        </option>)}
+      </select>
+      <input type='text' placeholder="Item.." />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList(){
+  return (
+    <div  className="list">
+      <ul>
+        {initialItems.map(item => <Item item={item}/>)}
+      </ul>
+    </div>
+    );
+}
+
+function Item({item}) {
+  return <li>
+      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
+        {item.description} {item.quantity} 
+      </span>
+      <button>❎</button> 
+    </li>
+}
+
+function Stats(){
+  return (
+    <footer className="stats"> <em> 🛄🛅🛅 You have X items on your list, and you already packed X (X%) </em> </footer>
+  );
+}
